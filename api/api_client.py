cea16e83fc0ffb896588e3c4938f67511876f0fa
horseheadnebula <oceanchasm@gmail.com>
2026-03-14 18:14:08 +0300

    update project configuration, add base api client

---
+++ api/api_client.py
@@ -0,0 +1,57 @@
+from typing import Optional
+
+import requests
+from requests.adapters import HTTPAdapter
+from urllib3.util.retry import Retry
+
+
+class BaseApiClient:
+    def __init__(
+        self,
+        url: str,
+        timeout: tuple[int, int] = (3, 10),
+        retries: int = 3,
+        backoff_factor: float = 1.0,
+    ):
+        self.url = url
+        self.timeout = timeout
+        self.session = requests.Session()
+
+        RETRY_STATUSES = (500, 502, 503, 504)
+
+        retry_strategy = Retry(
+            total=retries,  # сколько попыток
+            backoff_factor=backoff_factor,  # задержка между повторами
+            status_forcelist=RETRY_STATUSES,  # когда делать retry
+            allowed_methods=["GET", "POST", "PUT", "DELETE"],
+        )
+
+        adapter = HTTPAdapter(max_retries=retry_strategy)
+
+        self.session.mount("http://", adapter)
+        self.session.mount("https://", adapter)
+
+    def _request(self, method: str, path: str, **kwargs):
+        full_url = f"{self.url}{path}"
+
+        response = self.session.request(
+            method=method,
+            url=full_url,
+            timeout=kwargs.pop("timeout", self.timeout),
+            **kwargs,
+        )
+
+        response.raise_for_status()
+        return response
+
+    def get(self, path: str, params: Optional[dict] = None):
+        return self._request("GET", path, params=params)
+
+    def post(self, path: str, json: dict):
+        return self._request("POST", path, json=json)
+
+    def put(self, path: str, json: dict):
+        return self._request("PUT", path, json=json)
+
+    def delete(self, path: str):
+        return self._request("DELETE", path)
--- config/settings.py
+++ config/settings.py
@@ -1,2 +1,13 @@
-BASE_URL = "https://petstore.swagger.io/v2"
+import os
+
+ENV = os.getenv("ENV", "dev")
+
+BASE_URLS = {
+    "dev": "https://petstore.swagger.io/v2",
+    "stage": "https://petstore.swagger.io/v2",
+    "prod": "https://petstore.swagger.io/v2",
+}
+
+BASE_URL = BASE_URLS[ENV]
+
 TIMEOUT = 5
